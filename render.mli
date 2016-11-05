d819fc42421d23ca646ac925f942f1a9a409a03a
Jerica Huang <jerica.huang@gmail.com>
2016-11-05 16:02:13 -0400

    Render .mli

---
+++ render.mli
@@ -0,0 +1,6 @@
+(** [board_to_image board] is an image from [board]. *)
+val board_to_image : Main.board -> unit
+
+(** [animate_board move board] is the image of the animation of the 
+  	move [move] upon board [board]. Also overlays game win/loss message*)
+val animate_board : Game.move direction -> Game.board -> unit
