566e6e845e7f8c2e5f26baaf74804abfcc3ea812
Michael Velez <MichaelVelez@dhcp-ccc-4203.eduroam.cornell.edu>
2016-11-27 01:00:44 -0500

    fix

--- render.mli
+++ render.mli
@@ -3,4 +3,4 @@ val board_to_image : Gameplay.board -> unit
 
 (** [animate_board move board] is the image of the animation of the 
   	move [move] upon board [board]. Also overlays game win/loss message*)
-val animate_board : Gameplay.move direction -> Gameplay.board -> unit
+val animate_board : Gameplay.move -> Gameplay.board -> unit
