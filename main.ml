b8da37856c82cebf27d968e5153a11e8d870e742
Lillyan <ldp54@cornell.edu>
2016-12-03 13:17:01 -0500

    pop

--- main.ml
+++ main.ml
@@ -121,20 +121,54 @@ let draw_empty_sq ctx i j =
   ctx##fillStyle <- convert_color empty_color;
   ctx##fillRect (float x, float y, float w, float h)
 
-let draw_sq ctx i j sq_v =
+let draw_sq time ctx i j sq_v =
   let (x, y, w, h) = square_dim i j in
   let sq_colors = map_sq_colors sq_v in
   let sq_val_str = string_of_int sq_v in
   let text_vals = map_text_size sq_v in
+  begin
+  (* ctx##fillStyle <- convert_color (fst sq_colors);
+  ctx##fillRect (float x, float y, float (w) *. 0.1, float (h) *. 0.1);
+  ctx##clearRect(float x, float y, float (w) *. 0.1, float (h) *. 0.1);
+  ctx##fillStyle <- convert_color (fst sq_colors);
+  ctx##fillRect (float x, float y, float (w) *. 0.2, float (h) *. 0.2);
+  ctx##clearRect(float x, float y, float (w) *. 0.2, float (h) *. 0.2);
+  ctx##fillStyle <- convert_color (fst sq_colors);
+  ctx##fillRect (float x, float y, float (w) *. 0.6, float (h) *. 0.6);
+  ctx##font <- js(Printf.sprintf "700 %dpx Clearsans, Arial" (15));
+  ctx##textAlign <- js("center");
+  ctx##fillStyle <- convert_color (snd sq_colors);
+  ctx##fillText (js(sq_val_str),
+                float x +. float sq_w /. 2.05,
+                float y +. float sq_h /. (snd text_vals) );
+  ctx##clearRect(float x, float y, float (w) *. 0.6, float (h) *. 0.6);
+  ctx##fillStyle <- convert_color (fst sq_colors);
+  ctx##fillRect (float x, float y, float (w) *. 0.8, float (h) *. 0.8);
+  ctx##font <- js(Printf.sprintf "700 %dpx Clearsans, Arial" (20));
+  ctx##textAlign <- js("center");
+  ctx##fillStyle <- convert_color (snd sq_colors);
+  ctx##fillText (js(sq_val_str),
+                float x +. float sq_w /. 2.05,
+                float y +. float sq_h /. (snd text_vals) );
+  ctx##clearRect(float x, float y, float (w) *. 0.8, float (h) *. 0.8);
+  ctx##fillStyle <- convert_color (fst sq_colors);
+  ctx##fillRect (float x, float y, float (w) *. 0.9, float (h) *. 0.9);
+  ctx##font <- js(Printf.sprintf "700 %dpx Clearsans, Arial" (20));
+  ctx##textAlign <- js("center");
+  ctx##fillStyle <- convert_color (snd sq_colors);
+  ctx##fillText (js(sq_val_str),
+                float x +. float sq_w /. 2.05,
+                float y +. float sq_h /. (snd text_vals) );
+  ctx##clearRect(float x, float y, float (w) *. 0.9, float (h) *. 0.9); *)
   ctx##fillStyle <- convert_color (fst sq_colors);
   ctx##fillRect (float x, float y, float w, float h);
-  (* ctx##font <- js(Printf.sprintf "%dpx Verdana" ); *)
   ctx##font <- js(Printf.sprintf "700 %dpx Clearsans, Arial" (fst text_vals));
   ctx##textAlign <- js("center");
   ctx##fillStyle <- convert_color (snd sq_colors);
   ctx##fillText (js(sq_val_str),
                 float x +. float sq_w /. 2.05,
                 float y +. float sq_h /. (snd text_vals) )
+end
 
 let draw_board ctx b =
   ctx##fillStyle <- (convert_color board_lines_color);
