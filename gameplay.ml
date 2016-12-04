cb1aba72e4ac111755e86bdd3ba7daca238bd47a
swaggie <amandac188@gmail.com>
2016-12-04 11:44:34 -0500

    80 lines stuff

--- gameplay.ml
+++ gameplay.ml
@@ -79,36 +79,49 @@ let rec is_valid_move_left b row col =
   if row = 0 then false else
   if is_empty_row b (row-1) col then is_valid_move_left b (row-1) col else
   if b.(row-1).(0) = None then true else
-  if b.(row-1).(0) <> None && b.(row-1).(1) = None && (b.(row-1).(2) <> None || b.(row-1).(3) <> None) then true else
-  if b.(row-1).(0) <> None && b.(row-1).(1) <> None && b.(row-1).(2) = None && b.(row-1).(3) <> None then true else
-  if is_valid_merge_horizontal b (row-1) 0 1 || is_valid_merge_horizontal b (row-1) 1 2 || is_valid_merge_horizontal b (row-1) 2 3
+  if b.(row-1).(0) <> None && b.(row-1).(1) = None 
+  && (b.(row-1).(2) <> None || b.(row-1).(3) <> None) then true 
+  else if b.(row-1).(0) <> None && b.(row-1).(1) <> None && b.(row-1).(2) = None 
+  && b.(row-1).(3) <> None then true 
+  else if is_valid_merge_horizontal b (row-1) 0 1 
+  || is_valid_merge_horizontal b (row-1) 1 2 
+  || is_valid_merge_horizontal b (row-1) 2 3
   then true else is_valid_move_left b (row-1) col
 
 let rec is_valid_move_right b row col =
   if row = 0 then false else
   if is_empty_row b (row-1) col then is_valid_move_right b (row-1) col else
   if b.(row-1).(3) = None then true else
-  if b.(row-1).(3) <> None && b.(row-1).(2) = None && (b.(row-1).(1) <> None || b.(row-1).(0) <> None) then true else
-  if b.(row-1).(3) <> None && b.(row-1).(2) <> None && b.(row-1).(1) = None && b.(row-1).(0) <> None then true else
-  if is_valid_merge_horizontal b (row-1) 0 1 || is_valid_merge_horizontal b (row-1) 1 2 || is_valid_merge_horizontal b (row-1) 2 3
+  if b.(row-1).(3) <> None && b.(row-1).(2) = None && (b.(row-1).(1) <> None
+  || b.(row-1).(0) <> None) then true else
+  if b.(row-1).(3) <> None && b.(row-1).(2) <> None && b.(row-1).(1) = None 
+  && b.(row-1).(0) <> None then true else
+  if is_valid_merge_horizontal b (row-1) 0 1 || is_valid_merge_horizontal b (row-1) 1 2 
+  || is_valid_merge_horizontal b (row-1) 2 3
   then true else is_valid_move_right b (row-1) col
 
 let rec is_valid_move_up b row col =
   if col = 0 then false else
   if is_empty_col b (col-1) row then is_valid_move_up b row (col-1) else
   if b.(0).(col-1) = None then true else
-  if b.(0).(col-1) <> None && b.(1).(col-1) = None && (b.(2).(col-1) <> None || b.(3).(col-1) <> None) then true else
-  if b.(0).(col-1) <> None && b.(1).(col-1) <> None && b.(2).(col-1) = None && b.(3).(col-1) <> None then true else
-  if is_valid_merge_vertical b (col-1) 0 1 || is_valid_merge_vertical b (col-1) 1 2 || is_valid_merge_vertical b (col-1) 2 3
+  if b.(0).(col-1) <> None && b.(1).(col-1) = None && (b.(2).(col-1) <> None 
+  || b.(3).(col-1) <> None) then true 
+  else if b.(0).(col-1) <> None && b.(1).(col-1) <> None 
+  && b.(2).(col-1) = None && b.(3).(col-1) <> None then true 
+  else if is_valid_merge_vertical b (col-1) 0 1 || is_valid_merge_vertical b (col-1) 1 2 
+  || is_valid_merge_vertical b (col-1) 2 3
   then true else is_valid_move_up b row (col-1)
 
 let rec is_valid_move_down b row col =
   if col = 0 then false else
   if is_empty_col b (col-1) row then is_valid_move_down b row (col-1) else
   if b.(3).(col-1) = None then true else
-  if b.(3).(col-1) <> None && b.(2).(col-1) = None && (b.(1).(col-1) <> None || b.(0).(col-1) <> None) then true else
-  if b.(3).(col-1) <> None && b.(2).(col-1) <> None && b.(1).(col-1) = None && b.(0).(col-1) <> None then true else
-  if is_valid_merge_vertical b (col-1) 0 1 || is_valid_merge_vertical b (col-1) 1 2 || is_valid_merge_vertical b (col-1) 2 3
+  if b.(3).(col-1) <> None && b.(2).(col-1) = None && (b.(1).(col-1) <> None 
+  || b.(0).(col-1) <> None) then true 
+  else if b.(3).(col-1) <> None && b.(2).(col-1) <> None && b.(1).(col-1) = None
+  && b.(0).(col-1) <> None then true 
+  else if is_valid_merge_vertical b (col-1) 0 1 || is_valid_merge_vertical b (col-1) 1 2 
+  || is_valid_merge_vertical b (col-1) 2 3
   then true else is_valid_move_down b row (col-1)
 
 
@@ -353,3 +366,4 @@ let get_greedy_move (st : staticState) : move =
       done
     done;
     fst (snd (List.nth (sort_moveList_scores !score_moves) 0))
+    
